fadd671d1aabbde048b43a293f2e1ed30f59725d
Douglas Silva <dogametal@hotmail.com>
2020-08-25 16:37:47 -0300

    Version_017_Class_Comparable

---
+++ src/entities/WithDraw.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class WithDraw implements Comparable<WithDraw> {
+
+	private String parameter;
+	private Double withdraw;
+
+	public WithDraw(String parameter, Double withdraw) {
+		this.parameter = parameter;
+		this.withdraw = withdraw;
+	}
+
+	public String getParameter() {
+		return parameter;
+	}
+
+	public void setParameter(String parameter) {
+		this.parameter = parameter;
+	}
+
+	public Double getWithdraw() {
+		return withdraw;
+	}
+
+	public void setWithdraw(Double withdraw) {
+		this.withdraw = withdraw;
+	}
+
+
+	@Override
+	public int compareTo(WithDraw other) {
+		// TODO Auto-generated method stub
+		return -parameter.compareTo(other.getParameter());
+	}
+	
+	
+}
